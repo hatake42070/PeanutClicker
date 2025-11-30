93a1e5b730ad5c81b4b2f4a5a3629b9e24c90363
Naoki Hatayama <naoki42070@gmail.com>
2025-12-01 00:12:16 +0900

    [update]設計の変更

---
+++ Assets/PeanutClicker/Sqripts/MoneyView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Numerics;
+
+public class MoneyView : MonoBehaviour
+{
+    [SerializeField] private Text moneyText;
+
+    void Start()
+    {
+        // イベントを購読する
+        GameManager.Instance.OnMoneyChanged += UpdateDisplay;
+        GameManager.Instance.OnMoneyChanged += UpdateDisplay;
+    }
+
+    void OnDestroy()
+    {
+        // 購読解除（メモリリーク防止）
+        if (GameManager.Instance != null)
+            GameManager.Instance.OnMoneyChanged -= UpdateDisplay;
+    }
+
+    private void UpdateDisplay(BigInteger currentMoney)
+    {
+        moneyText.text = currentMoney.ToString("C0");
+    }
+}
