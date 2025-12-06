f9ce891fb9b123f871db5ee7b35e3fb1cc37c790
Naoki Hatayama <naoki42070@gmail.com>
2025-12-06 23:07:57 +0900

    [clean]コメントを追加

--- Assets/PeanutClicker/Sqripts/BigIntegerExtension.cs
+++ Assets/PeanutClicker/Sqripts/BigIntegerExtension.cs
@@ -1,4 +1,4 @@
-﻿using System.Numerics;
+﻿using System.Numerics; // BigInteger用
 using System.Collections.Generic;
 
 namespace Assets.PeanutClicker.Sqripts
@@ -6,7 +6,23 @@ namespace Assets.PeanutClicker.Sqripts
     /// <summary>
     /// BigInteger型を拡張して、日本語の単位（万・億・兆…）を表示できるようにするクラス
     /// </summary>
-    internal class BigIntegerExtension
+    public static class BigIntegerExtension // 拡張メソッドを定義する場合はstaticクラスにする
     {
+        // 単位の配列（無量大数まで対応）
+        private static readonly string[] Units = new string[]
+        {
+        "", "万", "億", "兆", "京", "垓", "𥝱", "穣", "溝", "澗", "正", "載", "極", "恒河沙", "阿僧祇", "那由他", "不可思議", "無量大数"
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
+        public static string ToJpString(this BigInteger value, string format = "F2")
+        {
+            return value.ToString();
+        }
     }
 }
